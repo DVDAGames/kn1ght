43b6a0ed2d5509e6785ffad5742926963ddab054
Eric allen <eric@ericrallen.dev>
2025-01-18 09:05:42 -0500

    chore: working through the build gpt from scratch notebook

--- scripts/train-tokenizer.py
+++ scripts/train-tokenizer.py
@@ -1,3 +1,5 @@
+import argparse
+
 from tokenizers import Regex, Tokenizer
 from tokenizers.decoders import ByteLevel as ByteLevelDecoder
 from tokenizers.models import BPE
@@ -6,15 +8,46 @@ from tokenizers.pre_tokenizers import Split
 from tokenizers.processors import ByteLevel as ByteLevelProcessor
 from tokenizers.trainers import BpeTrainer
 
+from kn1ght.constants import SPECIAL_TOKENS
+
 TRAINING_DATA_PATH = "./.data/training_data.txt"
 OUTPUT_PATH = "./src/tokenizer"
+VOCAB_SIZE = 4096
 
-with open(TRAINING_DATA_PATH, "r") as f:
+# get args from command line
+parser = argparse.ArgumentParser()
+
+parser.add_argument(
+    "--training_data_path",
+    help="Path to the training data file",
+    default=TRAINING_DATA_PATH,
+)
+
+parser.add_argument(
+    "--output_path",
+    help="Path to save the tokenizer",
+    default=OUTPUT_PATH,
+)
+
+parser.add_argument(
+    "--vocab_size",
+    help="Size of the vocabulary",
+    default=VOCAB_SIZE,
+)
+
+args = parser.parse_args()
+
+
+with open(args.training_data_path, "r") as f:
+    print(f"Training kn1ght tokenizer with {args.training_data_path}...")
+
     lines = f.readlines()
 
-    tokenizer = Tokenizer(
-        BPE(unk_token="[UNK]", fuse_unk=True, continuing_subword_prefix="")
-    )
+    lines = [
+        SPECIAL_TOKENS["START"] + line.strip() + SPECIAL_TOKENS["END"] for line in lines
+    ]
+
+    tokenizer = Tokenizer(BPE(unk_token=SPECIAL_TOKENS["UNKNOWN"], fuse_unk=True))
 
     tokenizer.normalizer = NFD()
 
@@ -25,10 +58,14 @@ with open(TRAINING_DATA_PATH, "r") as f:
     tokenizer.post_processor = ByteLevelProcessor(trim_offsets=True)
     tokenizer.decoder = ByteLevelDecoder()
 
-    trainer = BpeTrainer(vocab_size=4096, show_progress=True)
+    trainer = BpeTrainer(
+        vocab_size=args.vocab_size,
+        show_progress=True,
+        special_tokens=list(SPECIAL_TOKENS.values()),
+    )
 
     tokenizer.train_from_iterator(lines, trainer=trainer)
 
-    tokenizer.save(f"{OUTPUT_PATH}/kn1ght-tokenizer.json")
+    tokenizer.save(f"{args.output_path}/kn1ght-tokenizer.json")
 
-    tokenizer.model.save(OUTPUT_PATH, "kn1ght")
+    tokenizer.model.save(args.output_path, "kn1ght")
