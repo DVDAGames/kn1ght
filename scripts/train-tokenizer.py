f6ce7fba7e69e2ae342ed31753ff16bf28128686
Eric allen <eric@ericrallen.dev>
2025-01-20 16:51:44 -0500

    feat: generate model from sample of training data

--- scripts/train-tokenizer.py
+++ scripts/train-tokenizer.py
@@ -1,5 +1,6 @@
 import argparse
 
+from datasets import load_from_disk
 from tokenizers import Regex, Tokenizer
 from tokenizers.decoders import ByteLevel as ByteLevelDecoder
 from tokenizers.models import BPE
@@ -10,7 +11,7 @@ from tokenizers.trainers import BpeTrainer
 
 from kn1ght.constants import SPECIAL_TOKENS
 
-TRAINING_DATA_PATH = "./.data/training_data.txt"
+TRAINING_DATA_PATH = "./.data/datasets/full/2025-01-20"
 OUTPUT_PATH = "./src/tokenizer"
 VOCAB_SIZE = 4096
 
@@ -37,35 +38,34 @@ parser.add_argument(
 
 args = parser.parse_args()
 
+dataset = load_from_disk(dataset_path=args.training_data_path)
 
-with open(args.training_data_path, "r") as f:
-    print(f"Training kn1ght tokenizer with {args.training_data_path}...")
+print(f"Training kn1ght tokenizer with {args.training_data_path}...")
 
-    lines = f.readlines()
+training_data = []
 
-    lines = [
-        SPECIAL_TOKENS["START"] + line.strip() + SPECIAL_TOKENS["END"] for line in lines
-    ]
+for x in dataset["train"].select_columns("PGN").to_list():
+    training_data.append(x["PGN"])
 
-    tokenizer = Tokenizer(BPE(unk_token=SPECIAL_TOKENS["UNKNOWN"], fuse_unk=True))
+tokenizer = Tokenizer(BPE(unk_token=SPECIAL_TOKENS["UNKNOWN"], fuse_unk=True))
 
-    tokenizer.normalizer = NFD()
+tokenizer.normalizer = NFD()
 
-    tokenizer.pre_tokenizer = Split(
-        pattern=Regex(r""" ?\d+\.|\. ?| ?[-\w]+|[#+]"""), behavior="isolated"
-    )
+tokenizer.pre_tokenizer = Split(
+    pattern=Regex(r""" ?\d+\.|\. ?| ?[-\w]+|[#+]\s+"""), behavior="isolated"
+)
 
-    tokenizer.post_processor = ByteLevelProcessor(trim_offsets=True)
-    tokenizer.decoder = ByteLevelDecoder()
+tokenizer.post_processor = ByteLevelProcessor(trim_offsets=True)
+tokenizer.decoder = ByteLevelDecoder()
 
-    trainer = BpeTrainer(
-        vocab_size=args.vocab_size,
-        show_progress=True,
-        special_tokens=list(SPECIAL_TOKENS.values()),
-    )
+trainer = BpeTrainer(
+    vocab_size=args.vocab_size,
+    show_progress=True,
+    special_tokens=list(SPECIAL_TOKENS.values()),
+)
 
-    tokenizer.train_from_iterator(lines, trainer=trainer)
+tokenizer.train_from_iterator(training_data, trainer=trainer)
 
-    tokenizer.save(f"{args.output_path}/kn1ght-tokenizer.json")
+tokenizer.save(f"{args.output_path}/kn1ght-tokenizer.json")
 
-    tokenizer.model.save(args.output_path, "kn1ght")
+tokenizer.model.save(args.output_path, "kn1ght")
